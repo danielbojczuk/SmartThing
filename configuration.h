547ea6c81567f0b23ab67447b76813f2b38772c1
Daniel Bojczuk <39830414+danielbojczuk@users.noreply.github.com>
2023-01-17 17:12:53 +0100

    Create configuration.h

---
+++ configuration.h
@@ -0,0 +1,15 @@
+extern const char cacert[];
+
+extern const char client_cert[];
+ 
+extern const char privkey[];
+
+extern const char* wifiSSID; 
+
+extern const char* wifiPassword;
+
+extern const char* mqttBroker;
+
+extern const int mqttPort;
+
+extern const char* deviceName;
