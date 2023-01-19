c23b2e0d19f1b5e7e557e5958a63fcd12beb75f4
Daniel Bojczuk <39830414+danielbojczuk@users.noreply.github.com>
2023-01-19 09:58:36 +0100

    Update MQTTClient.ino

--- MQTTClient.ino
+++ MQTTClient.ino
@@ -6,63 +6,142 @@
 
 WiFiClientSecure secureClient = WiFiClientSecure();
 PubSubClient mqttClient(secureClient);
-int returnCode;
+
 
 BearSSL::X509List cert(cacert);
 BearSSL::X509List client_crt(client_cert);
 BearSSL::PrivateKey key(privkey);
 
+const char *ntp1 = "time.windows.com";
+const char *ntp2 = "pool.ntp.org";
+
+bool buttonStatus = false;
+bool deviceStatus = false;
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.begin(wifiSSID, wifiPassword);
+  pinMode(D1, OUTPUT);
+  pinMode(D2, INPUT_PULLUP);
+  deviceOff();
+  pinMode(D3, OUTPUT);
+  ledOff();
+  connectWifi();
+  setTIme();
+  connectMqtt();
+}
+
+void loop() {
+  delay(100);
+  connectWifi();
+  connectMqtt();
+  ledOn();
+
+  controlButton();
+  mqttClient.loop();
+  if(deviceStatus) {
+    deviceOn();
+  } else {
+    deviceOff();
+  }  
+}
+
+void controlButton() {
+   if (isButtonPressed()) {
+     Serial.println("Button Pressed"); 
+    if(!buttonStatus) {
+      Serial.println("Device status changed");
+      deviceStatus = !deviceStatus;
+     }
+     buttonStatus = true;
+     return;
+   }
+   buttonStatus = false;
+}
+
+
+bool isButtonPressed() {
+  return digitalRead(D2) == LOW;
+}
+
+void ledOn() {
+  digitalWrite(D3, HIGH);
+}
+
+void ledOff() {
+  digitalWrite(D3, LOW);
+}
+
+void deviceOn() {
+  digitalWrite(D1, LOW);
+}
+
+void deviceOff() {
+  digitalWrite(D1, HIGH);
+}
+
 void connectMqtt()
-{ 
+{
   if(mqttClient.connected()) {
     return;
   }
-
+  ledOff();
   secureClient.setTrustAnchors(&cert);
   secureClient.setClientRSACert(&client_crt, &key);
- 
+
   mqttClient.setServer(mqttBroker, mqttPort);
- 
-  Serial.println("Connecting to IOT");
- 
+  mqttClient.setCallback(messageReceived);
+  
+  Serial.println("Connecting to IOT broker...");
+  mqttClient.setKeepAlive(1200);
+
   while (!mqttClient.connect(deviceName))
   {
     Serial.print(".");
     delay(1000);
   }
- 
+
   if (!mqttClient.connected()) {
     Serial.println("AWS IoT Timeout!");
     return;
   }
-     
-  Serial.println("AWS IoT Connected!");
+
+  bool subscribed = mqttClient.subscribe("$aws/things/SmartLamp1/shadow/update/delta");
+  if(subscribed) {
+    Serial.println("subscribed to topic");
+  }
+
+  Serial.println("Connected to MQTT Broker!");
 }
 
 void connectWifi() {
   if(WiFi.status() == WL_CONNECTED) {
     return;
   }
-
-  Serial.print("Connecting.");
+  ledOff();
+  Serial.print("Connecting to wifi.");
   while (WiFi.status() != WL_CONNECTED) {
      delay(500);
      Serial.print(".");
   }
 
-  Serial.println("WiFi connected:");
+  Serial.println("WiFi connected.");
   Serial.println(WiFi.localIP());
 }
 
-void setup() {
-  WiFi.begin(wifiSSID, wifiPassword);
-  Serial.begin(9600);
-  connectWifi();  
-  connectMqtt();
+void setTIme() {
+  Serial.println("Updating data and time... ");
+  configTime(0, 1, ntp1, ntp2);
 }
 
-void loop() {
-  delay(500);
-  connectWifi(); 
-  connectMqtt();
+void messageReceived(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Received [");
+  Serial.print(topic);
+  Serial.print("]: ");
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
 }
